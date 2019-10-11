f8a2275e0d9be241267a510d7f3bb7dbd67d307a
Wismy Seide <wismyseide@outlook.com>
2019-10-10 22:10:04 -0400

    10102019

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyC7oN1M651SIcn4FNI-7tJCJnyyRsQpR_M",
+        authDomain: "train-scheduler-fa9c6.firebaseapp.com",
+        databaseURL: "https://train-scheduler-fa9c6.firebaseio.com",
+        projectId: "train-scheduler-fa9c6",
+        storageBucket: "train-scheduler-fa9c6.appspot.com",
+        messagingSenderId: "4087802302",
+        appId: "1:4087802302:web:6fe0bb5f81e1a8ea80f1ef",
+        measurementId: "G-0N4L28B4BC"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
+
+    // A variable to reference the database.
+    var database = firebase.database();
+
+    // Variables for the onClick event
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
+
+    $("#addtrain").on("click", function(){
+        event.preventDefault();
+        // Storing and retreiving new train data
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        // Pushing to database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("form")[0].reset();
+    });
+
+    database.ref().on("child_added", function(childSnapshot) {
+        var nextArr;
+        var minAway;
+        // Chang year so first train comes before now
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+        // Difference between the current and firstTrain
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        // Minutes until next train
+        var minAway = childSnapshot.val().frequency - remainder;
+        // Next train time
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+
+        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+                "</td><td>" + childSnapshot.val().destination +
+                "</td><td>" + childSnapshot.val().frequency +
+                "</td><td>" + nextTrain + 
+                "</td><td>" + minAway + "</td></tr>");
+
+            // Handle the errors
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+    });
+
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").html(snapshot.val().name);
+        $("#email-display").html(snapshot.val().email);
+        $("#age-display").html(snapshot.val().age);
+        $("#comment-display").html(snapshot.val().comment);
+    });
+
